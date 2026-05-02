580cd087897af20cdcce6457910845dd06d2843d
mzerem18-cpu <mzerem18@gmail.com>
2026-05-03 01:34:14 +0300

    Update SocialMenu.m

--- SocialMenu.m
+++ SocialMenu.m
@@ -26,7 +26,7 @@
 
     UIStackView *mainStack = [[UIStackView alloc] init];
     mainStack.axis = UILayoutConstraintAxisVertical;
-    mainStack.spacing = 25;
+    mainStack.spacing = 28; // بۆشایی نێوانیانم کەمێک زیادکرد بۆ جوانی
     mainStack.alignment = UIStackViewAlignmentCenter;
     mainStack.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:mainStack];
@@ -37,19 +37,19 @@
         [mainStack.widthAnchor constraintEqualToAnchor:self.view.widthAnchor multiplier:0.85]
     ]];
 
-    // --- لۆگۆی کەسی ---
+    // --- لۆگۆی کەسی (مۆدێرن و گەورەتر) ---
     UIImageView *imageView = [[UIImageView alloc] init];
     imageView.contentMode = UIViewContentModeScaleAspectFill;
     imageView.translatesAutoresizingMaskIntoConstraints = NO;
-    imageView.layer.cornerRadius = 35;
+    imageView.layer.cornerRadius = 42; // بازنەیی تەواو
     imageView.clipsToBounds = YES;
-    imageView.layer.borderWidth = 1.5;
-    imageView.layer.borderColor = [UIColor whiteColor].CGColor;
+    imageView.layer.borderWidth = 2.0;
+    imageView.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.9].CGColor;
     [mainStack addArrangedSubview:imageView];
 
     [NSLayoutConstraint activateConstraints:@[
-        [imageView.widthAnchor constraintEqualToConstant:70],
-        [imageView.heightAnchor constraintEqualToConstant:70]
+        [imageView.widthAnchor constraintEqualToConstant:84],
+        [imageView.heightAnchor constraintEqualToConstant:84]
     ]];
 
     // هێنانی لۆگۆی تە
@@ -62,19 +62,19 @@
         }
     });
 
-    // --- دەقی پێشوازی ---
+    // --- دەقی پێشوازی (فۆنتی زەبەلاح و مۆدێرن) ---
     UILabel *titleLabel = [[UILabel alloc] init];
     titleLabel.text = @"Welcome to\nAshteMobile";
     titleLabel.numberOfLines = 2;
     titleLabel.textAlignment = NSTextAlignmentCenter;
-    titleLabel.font = [UIFont systemFontOfSize:32 weight:UIFontWeightBold];
+    titleLabel.font = [UIFont systemFontOfSize:34 weight:UIFontWeightHeavy]; // ستایلی نوێی ئەپڵ
     titleLabel.textColor = [UIColor whiteColor];
     [mainStack addArrangedSubview:titleLabel];
 
     // --- سۆشیاڵ میدیا ---
     UIStackView *socialStack = [[UIStackView alloc] init];
     socialStack.axis = UILayoutConstraintAxisVertical;
-    socialStack.spacing = 15;
+    socialStack.spacing = 16;
     socialStack.distribution = UIStackViewDistributionFillEqually;
     socialStack.translatesAutoresizingMaskIntoConstraints = NO;
     [mainStack addArrangedSubview:socialStack];
@@ -83,7 +83,7 @@
         [socialStack.widthAnchor constraintEqualToAnchor:mainStack.widthAnchor]
     ]];
 
-    // دروستکردنی دوگمەکان بێ کێشە
+    // دروستکردنی دوگمەکان
     UIButton *tgBtn = [self createSocialButtonWithTitle:@"Telegram" color:[UIColor colorWithRed:0.2 green:0.6 blue:1.0 alpha:1.0] imageUrl:@"https://img.icons8.com/color/96/telegram-app.png" action:@selector(openTelegram)];
     UIButton *ttBtn = [self createSocialButtonWithTitle:@"TikTok" color:[UIColor whiteColor] imageUrl:@"https://img.icons8.com/fluent/96/tiktok.png" action:@selector(openTikTok)];
     UIButton *webBtn = [self createSocialButtonWithTitle:@"Website" color:[UIColor colorWithRed:0.8 green:0.4 blue:1.0 alpha:1.0] imageUrl:@"https://img.icons8.com/fluency/96/web.png" action:@selector(openWebsite)];
@@ -92,14 +92,21 @@
     [socialStack addArrangedSubview:ttBtn];
     [socialStack addArrangedSubview:webBtn];
 
-    // --- دوگمەی دەستپێکرن (Get Started) ---
+    // --- دوگمەی دەستپێکرن (Get Started بە سێبەر و ستایلی Capsule) ---
     UIButton *closeBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    closeBtn.backgroundColor = [UIColor colorWithRed:0.4 green:0.46 blue:0.98 alpha:1.0];
-    closeBtn.layer.cornerRadius = 20;
+    closeBtn.backgroundColor = [UIColor colorWithRed:0.1 green:0.45 blue:0.95 alpha:1.0]; // شینێکی زۆر تایبەت
+    closeBtn.layer.cornerRadius = 30; // شێوەی کەپسول (نیوەی بەرزییەکەی)
     [closeBtn setTitle:@"Get Started" forState:UIControlStateNormal];
     [closeBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    closeBtn.titleLabel.font = [UIFont systemFontOfSize:18 weight:UIFontWeightBold];
+    closeBtn.titleLabel.font = [UIFont systemFontOfSize:19 weight:UIFontWeightBold];
     [closeBtn addTarget:self action:@selector(closeTapped) forControlEvents:UIControlEventTouchUpInside];
+    
+    // زیادکردنی سێبەر بۆ دوگمەی سەرەکی
+    closeBtn.layer.shadowColor = [UIColor colorWithRed:0.1 green:0.45 blue:0.95 alpha:1.0].CGColor;
+    closeBtn.layer.shadowOpacity = 0.4;
+    closeBtn.layer.shadowOffset = CGSizeMake(0, 5);
+    closeBtn.layer.shadowRadius = 12;
+
     [mainStack addArrangedSubview:closeBtn];
 
     [NSLayoutConstraint activateConstraints:@[
@@ -108,16 +115,21 @@
     ]];
 }
 
-// فەنکشنی دروستکردنی دوگمە (گۆڕدرا بۆ شێوازی سەلامەت بۆ ڕێگریکردن لە ئیرۆری گیت هاب)
+// فەنکشنی دروستکردنی دوگمەی سۆشیاڵ میدیا بێ کێشە
 - (UIButton *)createSocialButtonWithTitle:(NSString *)title color:(UIColor *)color imageUrl:(NSString *)imageUrl action:(SEL)action {
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    btn.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.15]; // ڕەنگێکی شوشەیی جوان
-    btn.layer.cornerRadius = 15;
+    btn.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.1]; // ڕوونتر
+    btn.layer.cornerRadius = 18;
+    
+    // چوارچێوەیەکی زۆر تەنکی مۆدێرن بۆ جوانی
+    btn.layer.borderWidth = 0.5;
+    btn.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.2].CGColor;
+    
     [btn addTarget:self action:action forControlEvents:UIControlEventTouchUpInside];
     
     UIStackView *contentStack = [[UIStackView alloc] init];
     contentStack.axis = UILayoutConstraintAxisHorizontal;
-    contentStack.spacing = 10;
+    contentStack.spacing = 14;
     contentStack.alignment = UIStackViewAlignmentCenter;
     contentStack.translatesAutoresizingMaskIntoConstraints = NO;
     contentStack.userInteractionEnabled = NO;
@@ -126,7 +138,7 @@
     [NSLayoutConstraint activateConstraints:@[
         [contentStack.centerXAnchor constraintEqualToAnchor:btn.centerXAnchor],
         [contentStack.centerYAnchor constraintEqualToAnchor:btn.centerYAnchor],
-        [contentStack.heightAnchor constraintEqualToConstant:30]
+        [contentStack.heightAnchor constraintEqualToConstant:32]
     ]];
     
     UIImageView *iconView = [[UIImageView alloc] init];
@@ -135,8 +147,8 @@
     [contentStack addArrangedSubview:iconView];
     
     [NSLayoutConstraint activateConstraints:@[
-        [iconView.widthAnchor constraintEqualToConstant:25],
-        [iconView.heightAnchor constraintEqualToConstant:25]
+        [iconView.widthAnchor constraintEqualToConstant:26],
+        [iconView.heightAnchor constraintEqualToConstant:26]
     ]];
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
@@ -151,11 +163,11 @@
     UILabel *label = [[UILabel alloc] init];
     label.text = title;
     label.textColor = color;
-    label.font = [UIFont systemFontOfSize:17 weight:UIFontWeightSemibold];
+    label.font = [UIFont systemFontOfSize:17 weight:UIFontWeightBold]; // فۆنتی دوگمەکانم ئەستوورتر کرد
     [contentStack addArrangedSubview:label];
     
     [NSLayoutConstraint activateConstraints:@[
-        [btn.heightAnchor constraintEqualToConstant:55]
+        [btn.heightAnchor constraintEqualToConstant:58]
     ]];
     return btn;
 }
@@ -180,12 +192,11 @@
 @end
 
 // ------------------------------------------------------------------
-// بەشێ ئینجێکتکرنێ (بێ ئیرۆری Deprecated)
+// بەشێ ئینجێکتکرنێ 
 // ------------------------------------------------------------------
 __attribute__((constructor)) static void showCustomWelcomeScreen() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-        // ڕێگریکردن لە ئیرۆری گیت هاب بۆ کۆدە کۆنەکان
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
         UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
