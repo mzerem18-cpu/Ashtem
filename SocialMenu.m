89f42c0ae5a9ec05479178fb3f709ba03da89632
mzerem18-cpu <mzerem18@gmail.com>
2026-05-03 04:59:15 +0300

    Update SocialMenu.m

--- SocialMenu.m
+++ SocialMenu.m
@@ -1,10 +1,8 @@
 #import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h> // زیادکراوە بۆ چارەسەری ئێرۆری سێبەر
 
-// --- دیزاینی زۆر مۆدێرن و پڕۆفیشناڵی AshteMobile بە مەرجی Full Screen ---
+// --- دیزاینی مۆدێرن و سەلامەت بۆ AshteMobile ---
 @interface AshteWelcomeViewController : UIViewController
 @property (nonatomic, strong) UIView *glassCard;
-@property (nonatomic, strong) UIVisualEffectView *blurView;
 - (void)openTelegram;
 - (void)openTikTok;
 - (void)openWebsite;
@@ -17,39 +15,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // شاشەکە بەتەواوی بێ سنوور
+    // شاشەکە بەتەواوی بێ سنوور دەکەین
     self.view.backgroundColor = [UIColor clearColor];
     self.modalPresentationStyle = UIModalPresentationOverFullScreen;
 
-    // باکگراوندی تەڵخی مۆدێرن بۆ هەموو شاشەکە
+    // باکگراوندی تەڵخی مۆدێرن (Dark Blur)
     UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
-    self.blurView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-    self.blurView.frame = self.view.bounds;
-    self.blurView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    self.blurView.alpha = 0; // بۆ ئەنیمەیشنی سەرەتا
-    [self.view addSubview:self.blurView];
+    UIVisualEffectView *blurView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    blurView.frame = self.view.bounds;
+    blurView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:blurView];
 
-    // دروستکردنی کارتی شوشەیی (Glass Card)
+    // دروستکردنی کارتی شوشەیی (Dark Glass Card) - زۆر پڕۆفیشناڵتر
     self.glassCard = [[UIView alloc] init];
-    self.glassCard.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.12];
-    self.glassCard.layer.cornerRadius = 38; 
-    self.glassCard.layer.borderWidth = 1.5; 
-    self.glassCard.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.25].CGColor;
-    
-    // سێبەری درەوشاوە (Glow Effect)ی زۆر پڕۆفیشناڵ
-    self.glassCard.layer.shadowColor = [UIColor colorWithRed:0.0 green:0.5 blue:1.0 alpha:1.0].CGColor;
-    self.glassCard.layer.shadowOffset = CGSizeMake(0, 10);
-    self.glassCard.layer.shadowRadius = 30;
-    self.glassCard.layer.shadowOpacity = 0.4;
-    
+    self.glassCard.backgroundColor = [UIColor colorWithWhite:0.1 alpha:0.45]; // ڕەشییەکی شوشەیی جوان
+    self.glassCard.layer.cornerRadius = 35;
+    self.glassCard.layer.borderWidth = 1.0;
+    self.glassCard.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.15].CGColor;
     self.glassCard.translatesAutoresizingMaskIntoConstraints = NO;
-    self.glassCard.alpha = 0;
-    
-    // ئامادەکردن بۆ ئەنیمەیشنی (Spring)
-    self.glassCard.transform = CGAffineTransformMakeScale(0.8, 0.8);
     
     [self.view addSubview:self.glassCard];
     
+    self.glassCard.alpha = 0;
+
     [NSLayoutConstraint activateConstraints:@[
         [self.glassCard.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
         [self.glassCard.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor],
@@ -58,31 +46,31 @@
 
     UIStackView *mainStack = [[UIStackView alloc] init];
     mainStack.axis = UILayoutConstraintAxisVertical;
-    mainStack.spacing = 28; 
+    mainStack.spacing = 25;
     mainStack.alignment = UIStackViewAlignmentCenter;
     mainStack.translatesAutoresizingMaskIntoConstraints = NO;
     [self.glassCard addSubview:mainStack];
 
     [NSLayoutConstraint activateConstraints:@[
-        [mainStack.topAnchor constraintEqualToAnchor:self.glassCard.topAnchor constant:35],
-        [mainStack.bottomAnchor constraintEqualToAnchor:self.glassCard.bottomAnchor constant:-35],
+        [mainStack.topAnchor constraintEqualToAnchor:self.glassCard.topAnchor constant:30],
+        [mainStack.bottomAnchor constraintEqualToAnchor:self.glassCard.bottomAnchor constant:-30],
         [mainStack.leadingAnchor constraintEqualToAnchor:self.glassCard.leadingAnchor constant:20],
         [mainStack.trailingAnchor constraintEqualToAnchor:self.glassCard.trailingAnchor constant:-20]
     ]];
 
-    // --- لۆگۆی کەسی بە ستایلی نوێ ---
+    // --- لۆگۆی کەسی ---
     UIImageView *imageView = [[UIImageView alloc] init];
     imageView.contentMode = UIViewContentModeScaleAspectFill;
     imageView.translatesAutoresizingMaskIntoConstraints = NO;
-    imageView.layer.cornerRadius = 45; 
+    imageView.layer.cornerRadius = 40; 
     imageView.clipsToBounds = YES;
-    imageView.layer.borderWidth = 2.5;
-    imageView.layer.borderColor = [UIColor colorWithWhite:1.0 alpha:0.8].CGColor;
+    imageView.layer.borderWidth = 2.0;
+    imageView.layer.borderColor = [UIColor whiteColor].CGColor;
     [mainStack addArrangedSubview:imageView];
 
     [NSLayoutConstraint activateConstraints:@[
-        [imageView.widthAnchor constraintEqualToConstant:90],
-        [imageView.heightAnchor constraintEqualToConstant:90]
+        [imageView.widthAnchor constraintEqualToConstant:80],
+        [imageView.heightAnchor constraintEqualToConstant:80]
     ]];
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
@@ -94,97 +82,69 @@
         }
     });
 
-    // --- تایتڵ و سەبتایتڵ ---
-    UIStackView *titleStack = [[UIStackView alloc] init];
-    titleStack.axis = UILayoutConstraintAxisVertical;
-    titleStack.spacing = 4;
-    titleStack.alignment = UIStackViewAlignmentCenter;
-    [mainStack addArrangedSubview:titleStack];
-
+    // --- تایتڵ بە فۆنتێکی ئەستوورتر و جوانتر ---
     UILabel *titleLabel = [[UILabel alloc] init];
     titleLabel.text = @"AshteMobile";
-    titleLabel.font = [UIFont systemFontOfSize:32 weight:UIFontWeightBlack]; 
+    titleLabel.font = [UIFont systemFontOfSize:32 weight:UIFontWeightBlack];
     titleLabel.textColor = [UIColor whiteColor];
-    [titleStack addArrangedSubview:titleLabel];
-    
-    UILabel *subTitleLabel = [[UILabel alloc] init];
-    subTitleLabel.text = @"Premium Gaming Experience";
-    subTitleLabel.font = [UIFont systemFontOfSize:14 weight:UIFontWeightMedium];
-    subTitleLabel.textColor = [UIColor colorWithWhite:1.0 alpha:0.6];
-    [titleStack addArrangedSubview:subTitleLabel];
+    [mainStack addArrangedSubview:titleLabel];
 
     // --- سۆشیاڵ میدیا ---
     UIStackView *rowStack = [[UIStackView alloc] init];
     rowStack.axis = UILayoutConstraintAxisHorizontal;
     rowStack.spacing = 15;
-    rowStack.distribution = UIStackViewDistributionFillEqually;
     [mainStack addArrangedSubview:rowStack];
 
-    UIButton *tgBtn = [self createModernButton:@"Telegram" color:[UIColor colorWithRed:0.0 green:0.55 blue:1.0 alpha:1.0] action:@selector(openTelegram)];
-    UIButton *ttBtn = [self createModernButton:@"TikTok" color:[UIColor colorWithWhite:1.0 alpha:0.2] action:@selector(openTikTok)];
+    // ڕەنگەکانم گۆڕیوە بۆ ڕەنگی ئۆرجیناڵی ئەپەکان
+    UIButton *tgBtn = [self createModernButton:@"Telegram" color:[UIColor colorWithRed:0.15 green:0.65 blue:0.90 alpha:1.0] action:@selector(openTelegram)];
+    UIButton *ttBtn = [self createModernButton:@"TikTok" color:[UIColor colorWithWhite:0.1 alpha:1.0] action:@selector(openTikTok)];
     
     [rowStack addArrangedSubview:tgBtn];
     [rowStack addArrangedSubview:ttBtn];
 
-    [NSLayoutConstraint activateConstraints:@[
-        [rowStack.widthAnchor constraintEqualToAnchor:mainStack.widthAnchor]
-    ]];
-
-    UIButton *webBtn = [self createModernButton:@"Visit Official Website" color:[UIColor colorWithRed:0.95 green:0.15 blue:0.4 alpha:1.0] action:@selector(openWebsite)];
+    UIButton *webBtn = [self createModernButton:@"Visit Website" color:[UIColor colorWithRed:0.95 green:0.2 blue:0.4 alpha:1.0] action:@selector(openWebsite)];
     [mainStack addArrangedSubview:webBtn];
     
     [NSLayoutConstraint activateConstraints:@[
         [webBtn.widthAnchor constraintEqualToAnchor:mainStack.widthAnchor]
     ]];
 
-    // --- دوگمەی Get Started (ستایلی داپۆشین) ---
+    // --- دوگمەی Get Started (سپی بە تێکستی ڕەش، زۆر شازە) ---
     UIButton *closeBtn = [UIButton buttonWithType:UIButtonTypeSystem];
     closeBtn.backgroundColor = [UIColor whiteColor];
-    closeBtn.layer.cornerRadius = 22;
-    [closeBtn setTitle:@"Let's Go! ➔" forState:UIControlStateNormal];
-    [closeBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal]; 
+    closeBtn.layer.cornerRadius = 20;
+    [closeBtn setTitle:@"Get Started ➔" forState:UIControlStateNormal];
+    [closeBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     closeBtn.titleLabel.font = [UIFont systemFontOfSize:19 weight:UIFontWeightHeavy];
-    
-    // سێبەری دوگمەکە
-    closeBtn.layer.shadowColor = [UIColor whiteColor].CGColor;
-    closeBtn.layer.shadowOffset = CGSizeMake(0, 5);
-    closeBtn.layer.shadowRadius = 15;
-    closeBtn.layer.shadowOpacity = 0.5;
-    
     [closeBtn addTarget:self action:@selector(closeTapped) forControlEvents:UIControlEventTouchUpInside];
     [mainStack addArrangedSubview:closeBtn];
 
     [NSLayoutConstraint activateConstraints:@[
-        [closeBtn.widthAnchor constraintEqualToAnchor:mainStack.widthAnchor multiplier:0.95],
-        [closeBtn.heightAnchor constraintEqualToConstant:58]
+        [closeBtn.widthAnchor constraintEqualToAnchor:mainStack.widthAnchor multiplier:0.9],
+        [closeBtn.heightAnchor constraintEqualToConstant:55]
     ]];
 }
 
-// ئەنیمەیشنی هاتنە ژوورەوەی زۆر پڕۆفیشناڵ (Spring Animation)
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    
-    [UIView animateWithDuration:0.3 animations:^{
-        self.blurView.alpha = 1.0;
-    }];
-    
-    [UIView animateWithDuration:0.6 delay:0.1 usingSpringWithDamping:0.7 initialSpringVelocity:0.5 options:UIViewAnimationOptionCurveEaseOut animations:^{
+    [UIView animateWithDuration:0.5 animations:^{
         self.glassCard.alpha = 1.0;
-        self.glassCard.transform = CGAffineTransformIdentity;
-    } completion:nil];
+    }];
 }
 
 - (UIButton *)createModernButton:(NSString *)title color:(UIColor *)color action:(SEL)action {
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
     btn.backgroundColor = color;
     btn.layer.cornerRadius = 18;
-    
     [btn setTitle:title forState:UIControlStateNormal];
     [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     btn.titleLabel.font = [UIFont systemFontOfSize:15 weight:UIFontWeightBold];
     [btn addTarget:self action:action forControlEvents:UIControlEventTouchUpInside];
     
-    [btn.heightAnchor constraintEqualToConstant:52].active = YES;
+    [NSLayoutConstraint activateConstraints:@[
+        [btn.heightAnchor constraintEqualToConstant:50],
+        [btn.widthAnchor constraintGreaterThanOrEqualToConstant:120]
+    ]];
     
     return btn;
 }
@@ -198,23 +158,16 @@
 - (void)openTikTok { [self playHaptic]; [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.tiktok.com/@ashtemobile"] options:@{} completionHandler:nil]; }
 - (void)openWebsite { [self playHaptic]; [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://ashtemobile.tututweak.com/"] options:@{} completionHandler:nil]; }
 
-// ئەنیمەیشنی داخستن
 - (void)closeTapped {
     [self playHaptic];
-    [UIView animateWithDuration:0.4 animations:^{
-        self.glassCard.transform = CGAffineTransformMakeScale(0.8, 0.8);
-        self.glassCard.alpha = 0;
-        self.blurView.alpha = 0;
-    } completion:^(BOOL finished) {
-        [self dismissViewControllerAnimated:NO completion:nil];
-    }];
+    [self dismissViewControllerAnimated:NO completion:nil];
 }
 
 @end
 
 // --- بەشی ئینجێکتکردن ---
 __attribute__((constructor)) static void showCustomWelcomeScreen() {
-    // تێبینی: وشەی _Nonnull لێرە سڕایەوە بۆ چارەسەری کێشەی گیت‌هاب
+    // تێبینی: لێرەدا وشەی ئێرۆرەکە (Nonnull) سڕایەوە بۆ ئەوەی بێ کێشە بێت
     [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
         static dispatch_once_t onceToken;
         dispatch_once(&onceToken, ^{
